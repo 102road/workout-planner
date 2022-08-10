69451c2efe123ff3b90da05dd41a45dc0beb2a86
102road <bill.brown094@gmail.com>
2022-08-10 14:14:13 +0100

    add functions to create dropdown select using fetch list of body parts

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ce38d7e0e2mshf57ca5c97f2edcfp1a3c3ejsn84444674f0ed',
-		'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.API_KEY,
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
 };
 
-fetch('https://exercisedb.p.rapidapi.com/exercises', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// Get all body parts
+// fetch("https://exercisedb.p.rapidapi.com/exercises/bodyPartList", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+// // Get all exercises
+// fetch("https://exercisedb.p.rapidapi.com/exercises", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+// // Get exercise by body part
+// fetch(
+//   "https://exercisedb.p.rapidapi.com/exercises/bodyPart/%7BbodyPart%7D",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+//Create Select opttions Function
+const createOption = (item, index) => {
+  const option = document.createElement("option");
+  option.setAttribute("value", index);
+  option.innerHTML = item;
+  
+  const select = document.querySelector(".select");
+
+  select.append(option);
+};
+
+//Create drop down menu function
+const createMenu = () => {
+  const data = fetch(
+    "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      response.map((item, index) => createOption(item, index));
+    })
+    .catch((err) => console.error(err));
+};
+
+createMenu();
