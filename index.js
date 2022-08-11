e52b92fd2db2774de416df0181cd250c99cc8e89
102road <bill.brown094@gmail.com>
2022-08-11 16:45:47 +0100

    add button to exercise to allow it to be added to the schedule, other minor changes

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const options = {
 };
 
 const parentEl = document.querySelector(".main");
+const scheduleEl = document.querySelector(".schedule");
 let exerciseList = [];
-let bodyPart = '';
+let bodyPart = "";
 let listSizeIndicator = 0;
+let schedule = [];
 
 //Functions that create elements
-const createOption = (item, index) => {
+const createOption = (item) => {
   const option = document.createElement("option");
   option.setAttribute("value", item);
   option.innerHTML = item;
@@ -23,8 +25,9 @@ const createOption = (item, index) => {
   select.append(option);
 };
 
-const createExerciseItem = (item) => {
+const createExerciseItem = (item, index, option) => {
   const card = document.createElement("article");
+  card.setAttribute("class", `article ${item.name}`);
 
   const title = document.createElement("h1");
   title.setAttribute("class", "name");
@@ -35,11 +38,32 @@ const createExerciseItem = (item) => {
 
   const muscle = document.createElement("p");
   muscle.setAttribute("class", "muscle");
-  muscle.innerHTML = item.target;
+  muscle.innerHTML = `Target muscle: ${item.target}`;
 
   const equipment = document.createElement("p");
   equipment.setAttribute("class", "equipment");
-  equipment.innerHTML = item.equipment;
+  equipment.innerHTML = `Equipment needed:${item.equipment}`;
+
+  // Button is given a value of index so the exercise can be found within the array so it can be added to the schedule
+  if (!option) {
+    const addButton = document.createElement("button");
+    addButton.setAttribute("class", "add");
+    addButton.setAttribute("value", index);
+    addButton.innerHTML = "Add";
+    card.append(addButton);
+    addButton.addEventListener("click", (e) => {
+      const exerciseCardIndex = e.target.value;
+      const exercise = exerciseList[exerciseCardIndex];
+      schedule.push(exercise);
+      renderSchedule();
+    });
+  } else {
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "remove");
+    removeButton.setAttribute("value", index);
+    removeButton.innerHTML = "Remove";
+    card.appendChild(removeButton);
+  }
 
   card.append(title, gif, muscle, equipment);
   return card;
@@ -49,9 +73,15 @@ const createExerciseItem = (item) => {
 
 const renderExercises = () => {
   parentEl.innerHTML = "";
-  exerciseList.forEach((exercise) =>
-    parentEl.appendChild(createExerciseItem(exercise))
+  exerciseList.forEach((exercise, index) =>
+    parentEl.appendChild(createExerciseItem(exercise, index, false))
   );
+};
+
+const renderSchedule = () => {
+  schedule.forEach((exercise, index) => {
+    scheduleEl.appendChild(createExerciseItem(exercise, index, true));
+  });
 };
 
 const fetchExerciseData = () => {
@@ -61,10 +91,7 @@ const fetchExerciseData = () => {
   )
     .then((response) => response.json())
     .then((response) => {
-      exerciseList = response.slice(
-        listSizeIndicator,
-        listSizeIndicator + 49
-      );
+      exerciseList = response.slice(listSizeIndicator, listSizeIndicator + 49);
       renderExercises();
     })
     .catch((err) => console.error(err));
@@ -76,12 +103,22 @@ const fetchMenuData = () => {
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
-      response.map((item, index) => createOption(item, index));
+      response.map((item) => createOption(item));
     })
     .catch((err) => console.error(err));
 };
 
 fetchMenuData();
+
+// Event handlers
+
+const select = document.querySelector(".select");
+
+select.addEventListener("change", (e) => {
+  bodyPart = e.target.value;
+  console.log(bodyPart);
+  fetchExerciseData();
+});
 
 const button = document.querySelector(".button");
 
@@ -90,10 +127,12 @@ button.addEventListener("click", () => {
   fetchExerciseData();
 });
 
-const select = document.querySelector(".select");
+const previous = document.querySelector(".previous");
 
-select.addEventListener("change", (event) => {
-  bodyPart = event.target.value;
-  console.log(bodyPart);
+previous.addEventListener("click", () => {
+  if (!listSizeIndicator) {
+    listSizeIndicator = listSizeIndicator - 50;
+    return;
+  }
   fetchExerciseData();
 });
